91240a5a7d77c52bea879a31f192d945a7bb059e
Yasin Cakal <whoisyasin@gmail.com>
2025-04-06 21:12:11 -0700

    fixed layout

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,7 +7,7 @@ export default function Services() {
       <h2 className='section__title section__title--services'>Education</h2>
       <div className='services'>
         <div className='service'>
-          <h3>SUNY Albany</h3>
+          <h3>Diploma</h3>
           <p>
             Earned my Bachelor of Science in Computer Science from the
             University at Albany (SUNY) with Cum Laude honors and made the
@@ -17,17 +17,41 @@ export default function Services() {
         <div className='service'>
           <h3>Certifications</h3>
           <p>
-            Acquired more than 10 certifications in Front-End and Backend
-            Development from self-taught platforms such as Scrimba, Udemy, and
-            Codecademy.
+            Earned over 10 certifications in Front-End and Backend Development
+            from self-taught platforms such as Scrimba, Udemy, and Codecademy.
           </p>
         </div>
         <div className='service'>
-          <h3>Continuous Learning</h3>
+          <h3>Practice Daily</h3>
           <p>
-            Improve my skillset by consistently solving DSA challenges on
-            LeetCode and refining my front-end expertise through hands-on
-            projects on Frontend Mentor.
+            By consistently solving DSA challenges on LeetCode and refining my
+            front-end expertise through hands-on projects on Frontend Mentor.
+          </p>
+        </div>
+      </div>
+      <h2 className='section__title section__title--services'>
+        Skills & Qualifications
+      </h2>
+      <div className='services'>
+        <div className='service'>
+          <h3>Front-End</h3>
+          <p>
+            HTML, CSS, JavaScript, Figma (UI/UX), Tailwind, React, TypeScript,
+            Bootstrap, Bulma.
+          </p>
+        </div>
+        <div className='service'>
+          <h3>Backend</h3>
+          <p>
+            APIs, Express, Node, MongoDB, Next, SQL (MySQL, PostgreSQL), Python,
+            C, Java.
+          </p>
+        </div>
+        <div className='service'>
+          <h3>Other</h3>
+          <p>
+            Git, GitHub, Bash/Shell, AI/ML (OpenAI, Claude), Docker, Wordpress,
+            Agile, Linux.
           </p>
         </div>
       </div>
