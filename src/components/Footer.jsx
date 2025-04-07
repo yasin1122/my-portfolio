38034bc2f187b95be515c735b363f8f9864b031d
Yasin Cakal <whoisyasin@gmail.com>
2025-04-06 21:52:31 -0700

    contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,38 +2,38 @@
 export default function Footer() {
   return (
     <footer className='footer'>
-      <a
-        href='yasincakal@gmail.com'
-        className='footer__link'>
-        yasincakal@gmail.com
-      </a>
+      <h3>- Contact Me -</h3>
       <ul className='social-list'>
         <li className='social-list__item'>
           <a
             className='social-list__link'
-            href='https://codepen.io'>
-            <i className='fab fa-codepen'></i>
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://www.linkedin.com/in/yasincakal/'>
+            <i className='fab fa-linkedin'></i>
           </a>
         </li>
         <li className='social-list__item'>
           <a
             className='social-list__link'
-            href='http://dribbble.com'>
-            <i className='fab fa-dribbble'></i>
-          </a>
-        </li>
-        <li className='social-list__item'>
-          <a
-            className='social-list__link'
-            href='https://twitter.com'>
-            <i className='fab fa-twitter'></i>
-          </a>
-        </li>
-        <li className='social-list__item'>
-          <a
-            className='social-list__link'
-            href='https://github.com'>
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://github.com/yasin1122/'>
             <i className='fab fa-github'></i>
+          </a>
+        </li>
+        <li className='social-list__item'>
+          <a
+            className='social-list__link'
+            href='tel:3479274688'>
+            <i className='fas fa-phone'></i>
+          </a>
+        </li>
+        <li className='social-list__item'>
+          <a
+            className='social-list__link'
+            href='mailto:cakalyasin88@gmail.com'>
+            <i className='fas fa-envelope'></i>
           </a>
         </li>
       </ul>
